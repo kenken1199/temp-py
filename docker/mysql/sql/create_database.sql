abb8d3e5d78cf2ecd32bfc969116ec8312509656
kenken1199 <nakamori.k@gmail.com>
2024-09-01 14:38:57 +0900

    aa

--- docker/mysql/sql/create_database.sql
+++ docker/mysql/sql/create_database.sql
@@ -1,1 +1,3 @@
 create database raspi3;
+use raspi3;
+CREATE TABLE temperature (id int auto_increment primary key,date datetime NOT NULL,value float(8, 4) NOT NULL);
--- docker/mysql/sql/create_users_table.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE temperature (id int auto_increment primary key,date datetime NOT NULL,value float(8, 4) NOT NULL);
