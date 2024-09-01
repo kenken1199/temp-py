d454a47ec83ab59eb81af4804bee3cc46873aa9d
kenken1199 <nakamori.k@gmail.com>
2024-09-01 12:42:14 +0900

    add

---
+++ src/db.php
@@ -0,0 +1,15 @@
+<?php
+$servername="localhost";
+$username="kenta";
+$password="Kent@2428";
+$dbname="raspi3";
+
+$conn=new mysqli("$servername", "$username", "$password", "$dbname");
+
+if($conn){
+
+}else{
+    echo "Connection Failed";
+}
+
+?>
--- src/index.php
+++ src/index.php
@@ -1,3 +1,41 @@
-<?php
+<?php include "db.php";?>
+  <html>
+  <head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
 
-echo '<h1>Hello World</h1>';
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Date', 'Temperature'],
+          <?php
+	      $query="select * from temperature";
+	      $res=mysqli_query($conn,$query);
+	      while($data=mysqli_fetch_array($res)){
+		$date=$data['date'];
+		$temp=$data['value'];
+	  ?>
+	  ['<?php echo $date; ?>', <?php echo $temp; ?>],
+	  <?php
+	      }
+	  ?>
+
+        ]);
+
+        var options = {
+          title: 'My Room Temperature',
+          //curveType: 'function',
+          legend: { position: 'bottom' }
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+        chart.draw(data, options);
+      }
+    </script>
+  </head>
+  <body>
+    <div id="curve_chart" style="width: 900px; height: 500px"></div>
+  </body>
+</html>
